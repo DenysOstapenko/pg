d1d85a847e4f2c6b1de6415c7de98171e41ad2e4
DenysOstapenko <93434718+DenysOstapenko@users.noreply.github.com>
2022-02-12 15:16:22 +0100

    https://github.com/DenysOstapenko/pg.git

--- JS/game.js
+++ JS/game.js
@@ -13,6 +13,13 @@ fg.src = "img/fg.png";
 pipeUp.src = "img/pipeUp.png"; 
 pipeBottom.src = "img/pipeBottom.png"; 
 
+function draw(){
+    ctx.drawImage(bg, 0,0);
+
+    ctx.drawImage(pipeUp, 100, 0);
+    ctx.drawImage(pipeBottom, 100, 0 + pipeUp.height + gap);
+}
+
 
 var fly = new Audio();
 var score_audio = new Audio();
@@ -26,7 +33,7 @@ var gap = 90;
 document.addEventListener("keydown", moveUp);
 
 function moveUp() {
- yPos -= 25;
+ yPos -= 35;
  fly.play();
 }
 
@@ -53,7 +60,7 @@ function draw() {
 
  pipe[i].x--;
 
- if(pipe[i].x == 125) {
+ if(pipe[i].x == 100) {
  pipe.push({
  x : cvs.width,
  y : Math.floor(Math.random() * pipeUp.height) - pipeUp.height
@@ -80,7 +87,7 @@ function draw() {
 
  ctx.fillStyle = "#000";
  ctx.font = "24px Verdana";
- ctx.fillText("Счет: " + score, 10, cvs.height - 20);
+ ctx.fillText("SCORE: " + score, 10, cvs.height - 20);
 
  requestAnimationFrame(draw);
 }
